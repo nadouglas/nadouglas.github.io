d06b847dae824844dc3eefd86a41164c752ca7b0
nadouglas <ndzvf@missouri.edu>
2026-05-09 11:30:29 -0500

    Add files via upload

--- Gradproject/footer.php
+++ Gradproject/footer.php
@@ -1,13 +1,17 @@
-     <footer>
-      <span>Nabrittney Douglas, May 3, 2026</span>
+<footer class="footer">
 
-            <span>
-                <img src="phone-icon.png" alt="Phone icon">
-                901-000-1000
-            </span>
+    <span class="copyright">
+        © Nabrittney Douglas, May 8, 2026
+    </span>
 
-            <span>
-                <img src="email-icon.png" alt="Email icon">
-                nabrittney@gmail.com
-            </span>
-    </footer>
+    <span>
+        <img src="phone-icon.png" alt="Phone icon">
+        901-000-1000
+    </span>
+
+    <span>
+        <img src="email-icon.png" alt="Email icon">
+        nabrittney@gmail.com
+    </span>
+
+</footer>
